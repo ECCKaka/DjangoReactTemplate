18163ad2f4061d1021c6ef1d7ae344e1ac8fd92b
Chrisfufu <li491645426@gmail.com>
2019-12-04 00:39:55 -0700

    add user token :alien:

--- customers/api/serializers.py
+++ customers/api/serializers.py
@@ -1,8 +1,10 @@
 from rest_framework import serializers
 from customers.models import *
-
+from django.db.models import Q
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
+from rest_framework_jwt.settings import api_settings
+from django.db import transaction
 
 from rest_framework.serializers import (
     ModelSerializer,
@@ -13,6 +15,117 @@ from rest_framework.serializers import (
     HyperlinkedIdentityField,
     ListSerializer,
     )
+
+
+class UserCreateSerializer(ModelSerializer):
+    email = EmailField(label = 'Email')
+    email2 = EmailField(label = 'Confirm Email')
+    class Meta:
+        model = User
+        fields = [
+            'username',
+            'first_name',
+            'last_name',
+            'email',
+            'email2',
+            'password',
+        ]
+        extra_kwargs = {"password":
+                            {"write_only":True}
+                            }
+    def validate(self, data):
+        # email = data['email']
+        # user_qs = User.objects.filter(email= email)
+        # if user_qs.exists():
+        #     raise ValidationError("this user has already registered.")
+        return data
+
+    def validate_email(self, value):
+        data = self.get_initial()
+        email1 = data.get("email2")
+        email2 = value
+        if email1 != email2:
+            raise ValidationError("Emails must match")
+
+        user_qs = User.objects.filter(email= email2)
+        if user_qs.exists():
+            raise ValidationError("this user has already registered.")
+
+
+        return value
+
+    def validate_email2(self, value):
+        data = self.get_initial()
+        email1 = data.get("email")
+        email2 = value
+        if email1 != email2:
+            raise ValidationError("Emails must match")
+        return value
+    def create(self, validated_data):
+        username = validated_data['username']
+        email = validated_data['email']
+        password = validated_data['password']
+        user_obj = User(
+                username = username,
+                email = email
+        )
+        user_obj.set_password(password)
+        user_obj.save()
+        return validated_data
+
+class UserLoginSerializer(ModelSerializer):
+    '''
+        format of user login
+        {
+        "identifier": "admin",
+        "password": "admin"
+        }
+    '''
+    token = CharField(allow_blank=True, read_only=True)
+    identifier = CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = [
+            'identifier',
+            'password',
+            'token',
+        ]
+        extra_kwargs = {
+            "password": {
+                "write_only": True
+            }
+        }
+
+    def get_token(self, obj):
+        jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+        jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
+
+        payload = jwt_payload_handler(obj)
+        token = jwt_encode_handler(payload)
+        return token
+
+    def validate(self, data):
+        user = None
+        user_obj = None
+        identifier = data["identifier"]
+        password = data["password"]
+
+        if not identifier:
+            raise ValidationError("A username or email is required to login.")
+
+        user = User.objects.filter(Q(username = identifier) | Q(email = identifier)).distinct()
+
+        if user.exists() and user.count() == 1:
+            user_obj = user.first()
+        else:
+            raise ValidationError("This username/email is not valid.")
+
+        if user_obj:
+            if not user_obj.check_password(password):
+                raise ValidationError("Incorrect credentials please try again.")
+
+        return { 'token': self.get_token(user_obj) }
 
 class ProfileSerializer(ModelSerializer):
     class Meta:
--- myProject/settings.py
+++ myProject/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-
+import datetime
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -37,10 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Third party apps
     'rest_framework',
     'corsheaders',
-    'customers',
-    'livereload'
+    'livereload',
+
+    # Project apps
+    'customers'
 ]
 
 REST_FRAMEWORK = {
@@ -61,6 +65,14 @@ MIDDLEWARE = [
 
 MIDDLEWARE_CLASSES = (
     'livereload.middleware.LiveReloadScript',
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
 ROOT_URLCONF = 'myProject.urls'
@@ -138,3 +150,24 @@ CORS_ORIGIN_ALLOW_ALL = False
 CORS_ORIGIN_WHITELIST = (
        'http://localhost:3000',
 )
+
+# Setup Django REST Framework
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+       'rest_framework.permissions.IsAuthenticated'
+    ],
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ],
+}
+
+JWT_AUTH = {
+    'JWT_AUTH_HEADER_PREFIX': 'Bearer',
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(days=1), #TODO: need frontend to refresh once expired.
+    # 'JWT_REFRESH_EXPIRATION_DELTA': datetime.timedelta(seconds=3),
+}
