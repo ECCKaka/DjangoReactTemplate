7fb3808e8d317c5646de2f3e097cef07567ca7ca
Chrisfufu <zuofu@ualberta.ca>
2020-04-05 00:24:37 -0600

    add header

---
+++ src/components/header/index.js
@@ -0,0 +1,59 @@
+/**
+ * Header component.
+ */
+import React from 'react';
+import { useHistory } from "react-router-dom";
+
+import './style.css'
+
+// hearder url
+const iconImgUrl = process.env.PUBLIC_URL + '/django-logo-big.png'
+
+var iconStyle = {
+  pc: {
+    height: "8rem",
+    width: "8rem",
+    cursor: 'pointer'
+  },
+  mobile: {
+    height: "8rem",
+    width: "8rem",
+  }
+}
+
+var headerStyle = {
+  pc: {
+    height: "8rem",
+  },
+  mobile: {
+    height: "8rem",
+  }
+}
+
+
+function AppHeader() {
+  // const Window = WindowStore.useContainer()
+  // const Auth = AuthStore.useContainer()
+  let history = useHistory();
+
+  // if header clicked, return to home page
+  let handleIconOnClick = () => {
+    if (window.location.pathname !== '/home') {
+      history.push('/home')
+    }
+  }
+  // the following is the header logo
+  return (
+    <div className='layout-header'>
+      <img
+        onClick={handleIconOnClick}
+        className='layout-header-logo'
+        alt="icon"
+        src={iconImgUrl}>
+      </img>
+    </div>
+  )
+
+}
+
+export default AppHeader;
